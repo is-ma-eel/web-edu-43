f7039cadea1d5a242cb83ec9dae939fb135d27ff
ojecode <ojecode@gmail.com>
2020-10-13 21:06:50 +0300

    first commit

---
+++ assignment/src/MainRouters.js
@@ -0,0 +1,15 @@
+//All application routes go here
+import React from "react";
+import { Router, Route } from "react-router-dom";
+
+
+const createRoutes = () => 
+    (
+        <Router>
+            <Route>
+                 {/* /*componets rendered here*/ }
+            </Route>
+        </Router>
+    );
+
+export default createRoutes;
