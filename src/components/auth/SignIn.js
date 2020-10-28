d1f2ff71dbabe393026201d22a92e0943000180c
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2020-10-29 00:27:39 +0200

    Styles <Signin />

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import Navbar from '../layout/Navbar';
 import WelcomePage from '../layout/WelcomePage';
 import Footer from '../layout/Footer';
@@ -18,24 +18,21 @@ const SignIn = () => {
     mode: 'all',
     defaultValues: { email: 'John_Doe@gmail.com', password: 123456789 },
   });
-  const {handleSignin, inputs, setInputs} = useContext(firebaseAuth)
+  const { handleSignin, inputs, setInputs } = useContext(firebaseAuth);
   // const {handleSignup, inputs, setInputs, errors} = useContext(firebaseAuth)
 
-
-
   const handleChange = (e) => {
-    const {name, value} = e.target
-    console.log(inputs)
-    setInputs(prev => ({...prev, [name]: value}))
-  }
-
+    const { name, value } = e.target;
+    console.log(inputs);
+    setInputs((prev) => ({ ...prev, [name]: value }));
+  };
 
   const onSubmit = (data) => {
     /* note this redirect to studentdashboard after
       clicking on sign in is for expermentail purposes
       will be modified based on user type after authentication
     */
-    history.push('/studentdashboard');
+    // history.push('/studentdashboard');
     console.log(data);
     // empty form fields
     reset();
@@ -44,58 +41,62 @@ const SignIn = () => {
   return (
     <>
       <Navbar />
-        <div className="row sign">
-          <section className="col"> <WelcomePage /> </section>
+      <div className="row">
+        <section className="col">
+          <WelcomePage />
+        </section>
 
-          <section className="col form-container py-4">
-            <h2> Sign in</h2>
-            <form className="form" onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-row">
-                <span className="required-style">* </span>
-                <div className="col">
-                  <input
-                    aria-label="Enter your email"
-                    aria-required="true"
-                    placeholder="Email"
-                    className="form-text form-control"
-                    type="email"
-                    name="email"
-                    autoFocus
-                    ref={register({
-                      required: 'Email required'})}
-                  />
-                </div>
-                {errors.email && <p className="invalid-field">{errors.email.message}</p>}
+        <section className="col form-container py-4">
+          <h2> Sign in</h2>
+          <form className="form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-row">
+              <span className="required-style">* </span>
+              <div className="col mb-3">
+                <input
+                  aria-label="Enter your email"
+                  aria-required="true"
+                  placeholder="Email"
+                  className="form-text form-control"
+                  type="email"
+                  name="email"
+                  autoFocus
+                  ref={register({
+                    required: 'Email required',
+                  })}
+                />
               </div>
+              {errors.email && <p className="invalid-field">{errors.email.message}</p>}
+            </div>
 
-              <div className="form-row">
-                  <span className="required-style">* </span>
-                  <div className="col">
-                    <input
-                      aria-label="Enter your password"
-                      aria-required="true"
-                      placeholder="Password"
-                      className="form-text form-control"
-                      type="password"
-                      name="password"
-                      ref={register({
-                        required: 'Password required'})}
-                    />
-                  </div>
-                  {errors.password && <p className="invalid-field">{errors.password.message}</p>}
-                </div>                         
+            <div className="form-row">
+              <span className="required-style">* </span>
+              <div className="col mb-3">
+                <input
+                  aria-label="Enter your password"
+                  aria-required="true"
+                  placeholder="Password"
+                  className="form-text form-control"
+                  type="password"
+                  name="password"
+                  ref={register({
+                    required: 'Password required',
+                  })}
+                />
+              </div>
+              {errors.password && <p className="invalid-field">{errors.password.message}</p>}
+            </div>
 
-                <button className="sub-btn btn btn-secondary btn-lg" type="submit">
-                   Sign in
-                </button>
+            <button className="sub-btn btn btn-secondary btn-lg" type="submit">
+              Sign in
+            </button>
 
-                <p>
-                  Don't have an account yet? <Link to="/signup">Sign up</Link> for free
-                </p>
-              </form>
-            </section>
-          </div>
-          <Footer />
+            <p>
+              Don't have an account yet? <Link to="/signup">Sign up</Link> for free
+            </p>
+          </form>
+        </section>
+      </div>
+      <Footer />
     </>
   );
 };
--- src/components/auth/provider/AuthProvider.js
+++ src/components/auth/provider/AuthProvider.js
@@ -1,54 +1,45 @@
-import React, {useState} from 'react';
-import {authMethods} from '../firebase/authmethods';
-
+import React, { useState } from 'react';
+import { authMethods } from '../firebase/authmethods';
 
 export const firebaseAuth = React.createContext();
 
-
-
-
 const AuthProvider = (props) => {
-
   // State (token, input and error)
-  const [inputs, setInputs] = useState({email: '', password: ''})
-  const [errors, setErrors] = useState([])
-  const [token, setToken] = useState(null)
-
+  const [inputs, setInputs] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState([]);
+  const [token, setToken] = useState(null);
 
   const handleSignup = () => {
-    console.log('handleSignup')
-    authMethods.signup(inputs.email, inputs.password, setErrors, setToken)
-    console.log(errors, token)
-  }
+    console.log('handleSignup');
+    authMethods.signup(inputs.email, inputs.password, setErrors, setToken);
+    console.log(errors, token);
+  };
 
   const handleSignin = () => {
-    console.log('handleSignin!!!')
-    authMethods.signin(inputs.email, inputs.password, setErrors, setToken)
-    console.log(errors, token)
-  }
+    console.log('handleSignin!!!');
+    authMethods.signin(inputs.email, inputs.password, setErrors, setToken);
+    console.log(errors, token);
+  };
 
   const handleSignout = () => {
-    authMethods.signout()
-  }
+    authMethods.signout();
+  };
 
   return (
     <firebaseAuth.Provider
-    value={{
-      handleSignup,
-      handleSignin,
-      token,
-      inputs,
-      setInputs,
-      errors,
-      handleSignout,
-    }}>
+      value={{
+        handleSignup,
+        handleSignin,
+        token,
+        inputs,
+        setInputs,
+        errors,
+        handleSignout,
+      }}
+    >
       {props.children}
     </firebaseAuth.Provider>
   );
-
 };
-
-
-
 
 export default AuthProvider;
